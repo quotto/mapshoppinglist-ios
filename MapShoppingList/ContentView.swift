99c7031f99c2921f2b37d46311182257e14a87e1
quotto <5211139+quotto@users.noreply.github.com>
2025-10-29 00:20:08 +0900

    Initial Commit

---
+++ MapShoppingList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapShoppingList
+//
+//  Created by 山田貴彦 on 2025/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapShoppingList/MapShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapShoppingListApp.swift
+//  MapShoppingList
+//
+//  Created by 山田貴彦 on 2025/10/29.
+//
+
+import SwiftUI
+
+@main
+struct MapShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
