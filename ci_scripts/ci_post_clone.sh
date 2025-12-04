71b642ca12153d73b967e0ac4849d9e5165412fa
quotto <5211139+quotto@users.noreply.github.com>
2025-12-05 00:02:42 +0900

    refactor: XCode Cloud上でSPM Pluginが使えなかったため、デフォルト許可するように設定

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,5 +8,5 @@ VERSION_CONFIG_PATH="${SCRIPT_DIR}/../Config.version.xcconfig"
 
 set +x 2>/dev/null
 printf "GOOGLE_MAPS_API_KEY = %s\n" "${GOOGLE_MAPS_API_KEY}" >> "${SECRET_CONFIG_PATH}"
-printf "MARKETING_VERSION = 1.${CI_PULL_REQUEST_NUMBER}" >> "${VERSION_CONFIG_PATH}"
+printf "MARKETING_VERSION = 1.%s\n" "${CI_PULL_REQUEST_NUMBER:-0}" >> "${VERSION_CONFIG_PATH}"
 set -x 2>/dev/null
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+# SPMのプラグインをデフォルト許可しておく
+defaults write com.apple.dt.Xcode SwiftLintBuildToolPlugin -bool YES
